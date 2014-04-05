aacc36c404354e53d595c74f901ff719b4e92820
James Magnarelli <jmagnare@gmail.com>
2014-04-05 11:06:47 -0400

    Rename listener, add server to add new user, add flask to requirements

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request, session
+import pymongo
+
+
+app = Flask(__name__, static_url_path="")
+app.debug = True
+app.secret_key = 'WOW_SO_SECRET_ZOMG'
+
+db = pymongo.MongoClient().user_data
+
+@app.route('/status')
+def status():
+	return jsonify({'message': 'OK'})
+
+@app.route('/add-user', methods=['POST'])
+def add_user():
+
+	# Check for correct content type
+	if not request.json:
+		abort(400)
+
+	user = {'phone_number': request.json['phoneNumber'],
+			'region': request.json['region']}
+	db.users.insert(user)
+
+	return jsonify({'message': 'All good!'})
+
+if __name__ == '__main__':
+	app.run()
