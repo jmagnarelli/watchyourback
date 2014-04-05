18fa009aba3889b8cd852c724b7b17b27914e1db
James Magnarelli <jmagnare@gmail.com>
2014-04-05 12:25:07 -0400

    Change to run on port 80, serv status

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request, session
 import pymongo
 
 
-app = Flask(__name__, static_url_path="")
+app = Flask(__name__, static_url_path="static", static_folder="static")
 app.debug = True
 app.secret_key = 'WOW_SO_SECRET_ZOMG'
 
@@ -26,4 +26,4 @@ def add_user():
 	return jsonify({'message': 'All good!'})
 
 if __name__ == '__main__':
-	app.run()
+	app.run(port=80) # Yeah, I know the debug server shouldn't be used in prod. This is a hackathon.
